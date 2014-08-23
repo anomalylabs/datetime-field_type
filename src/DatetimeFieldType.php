b2ccd358d572f4eaf8177381949b9a81bf546f90
Ryan Thompson <service@aiwebsystems.com>
2014-08-22 20:42:15 -0500

    Initial commit.

---
+++ src/DatetimeFieldType.php
@@ -0,0 +1,30 @@
+<?php namespace Addon\FieldType\Datetime;
+
+use Streams\Core\Addon\FieldTypeAbstract;
+
+class DatetimeFieldType extends FieldTypeAbstract
+{
+    /**
+     * The database column type this field type uses.
+     *
+     * @var string
+     */
+    public $columnType = 'datetime';
+
+    /**
+     * Field type version
+     *
+     * @var string
+     */
+    public $version = '1.1.0';
+
+    /**
+     * Field type author information.
+     *
+     * @var array
+     */
+    public $author = array(
+        'name' => 'AI Web Systems, Inc.',
+        'url'  => 'http://aiwebsystems.com/',
+    );
+}
