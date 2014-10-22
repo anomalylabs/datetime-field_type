07e2496b2daacb24c5bd6078bbcdecb409ca23c5
Ryan Thompson <service@aiwebsystems.com>
2014-10-22 13:30:54 -0500

    Clean before pushing final

--- src/DatetimeFieldType.php
+++ src/DatetimeFieldType.php
@@ -4,29 +4,5 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
 class DatetimeFieldType extends FieldTypeAddon
 {
-    protected $slug = 'datetime';
-
-    /**
-     * The database column type this field type uses.
-     *
-     * @var string
-     */
     public $columnType = 'datetime';
-
-    /**
-     * Field type version
-     *
-     * @var string
-     */
-    public $version = '1.1.0';
-
-    /**
-     * Field type author information.
-     *
-     * @var array
-     */
-    public $author = array(
-        'name' => 'AI Web Systems, Inc.',
-        'url'  => 'http://aiwebsystems.com/',
-    );
 }
