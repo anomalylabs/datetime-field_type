06a4b94d573a15e05692fa9e46072d2f2411b07b
Ryan Thompson <service@aiwebsystems.com>
2014-11-06 14:27:32 -0600

    No need now

--- src/DatetimeFieldType.php
+++ src/DatetimeFieldType.php
@@ -7,16 +7,6 @@ class DatetimeFieldType extends FieldType
 
     protected $columnType = 'datetime';
 
-    protected function onSet($value)
-    {
-        if (is_numeric($value)) {
-
-            $value = date('Y-m-d H:i:s', $value);
-        }
-
-        return $value;
-    }
-
     public function isZeros()
     {
         return (starts_with($this->getValue(), '00'));
