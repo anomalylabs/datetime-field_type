14b30377513f095e5f822e2ed1c8166861fac9b1
Ryan Thompson <service@aiwebsystems.com>
2015-07-10 00:07:32 -0500

    Progress

--- resources/config/config.php
+++ resources/config/config.php
@@ -15,8 +15,20 @@ return [
         'type'   => 'anomaly.field_type.select',
         'config' => [
             'options' => [
-                'j F, Y|d MM, yy' => function () {
-                    return date('j F, Y');
+                'l, F j, Y|DD, d MM, yy' => function () {
+                    return date('l, F j, Y'); // Friday, 10 July, 2015
+                },
+                'j F, Y|d MM, yy'        => function () {
+                    return date('j F, Y'); // 10 July, 2015
+                },
+                'j M, y|d M, y'          => function () {
+                    return date('j M, y'); // 10 Jul, 15
+                },
+                'm/d/Y|mm/dd/yy'         => function () {
+                    return date('m/d/Y'); // 07/10/2015
+                },
+                'Y-m-d|mm/dd/yy'         => function () {
+                    return date('Y-m-d'); // 2015-07-10
                 }
             ]
         ]
@@ -26,7 +38,10 @@ return [
         'config' => [
             'options' => [
                 'g:i A' => function () {
-                    return date('g:00 A');
+                    return date('g:00 A'); // 4:00 PM
+                },
+                'G:i A' => function () {
+                    return date('G:00'); // 16:00
                 }
             ]
         ]
