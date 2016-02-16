3db70b624e011cd4001a1f17f9bfa7b98ef0ffb3
Ryan Thompson <ryan@pyrocms.com>
2016-02-16 14:14:24 -0600

    Tweaks

--- resources/js/input.js
+++ resources/js/input.js
@@ -2,6 +2,11 @@ $(function () {
 
     // Initialize date pickers
     $('.datetime-field_type .datepicker').each(function () {
+
+        $(this).prev('.icon').click(function() {
+            $(this).next('.datepicker').focus();
+        });
+
         $(this).datepicker({
             dateFormat: $(this).data('date-format'),
             yearRange: $(this).data('year-range'),
@@ -16,6 +21,11 @@ $(function () {
 
     // Initialize time pickers
     $('.datetime-field_type .timepicker').each(function () {
+
+        $(this).prev('.icon').click(function() {
+            $(this).next('.timepicker').focus();
+        });
+
         $(this).timepicker({
             timeFormat: $(this).data('time-format'),
             step: $(this).data('step'),
