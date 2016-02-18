4dbf30f52b61db8ac7d438f7136dd01448658b19
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 01:01:20 -0600

    Author

--- src/DatetimeFieldType.php
+++ src/DatetimeFieldType.php
@@ -8,9 +8,9 @@ use Illuminate\Config\Repository;
 /**
  * Class DatetimeFieldType
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\DatetimeFieldType
  */
 class DatetimeFieldType extends FieldType
