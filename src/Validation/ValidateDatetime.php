68c945f2ca7a63a9d7158e5928019f0f7d34796d
josebarbosa <joseasbarbosa@gmail.com>
2017-02-04 18:28:29 +0000

    bug fix datetime FT validation
    Validation fails when a form has more than one datetime FT with
    different formats.

--- src/Validation/ValidateDatetime.php
+++ src/Validation/ValidateDatetime.php
@@ -1,6 +1,6 @@
 <?php namespace Anomaly\DatetimeFieldType\Validation;
 
-use Anomaly\DatetimeFieldType\DatetimeFieldType;
+use Anomaly\Streams\Platform\Ui\Form\FormBuilder;
 use Carbon\Carbon;
 
 /**
@@ -16,12 +16,14 @@ class ValidateDatetime
     /**
      * Handle the validation.
      *
-     * @param DatetimeFieldType $fieldType
-     * @param                   $value
+     * @param FormBuilder   $builder
+     * @param               $attribute
+     * @param               $value
      * @return bool
      */
-    public function handle(DatetimeFieldType $fieldType, $value)
+    public function handle(FormBuilder $builder, $attribute, $value)
     {
+        $fieldType = $builder->getFormFieldFromAttribute($attribute);
         try {
             (new Carbon())->createFromFormat($fieldType->getDatetimeFormat(), $value);
         } catch (\Exception $e) {
