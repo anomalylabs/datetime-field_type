ca012d58dda837cbe7a4af47266b8bc01e69b7e0
Ryan Thompson <ryan@pyrocms.com>
2017-07-06 19:47:49 -0500

    Cache

--- resources/js/input.js
+++ resources/js/input.js
@@ -3,11 +3,13 @@ $(document).on('ajaxComplete ready', function () {
     // Initialize inputs
     $('input[data-provides="anomaly.field_type.datetime"]:not([data-initialized])').each(function () {
 
-        $(this).attr('data-initialized', '')
+        var $this = $(this);
+
+        $this.attr('data-initialized', '')
             .datetimeEntry({
                 spinnerImage: '',
-                timeSteps: [1, $(this).data('step')],
-                datetimeFormat: $(this).data('datetime-format')
+                timeSteps: [1, $this.data('step')],
+                datetimeFormat: $this.data('datetime-format')
             });
     });
 });
--- resources/js/picker.js
+++ resources/js/picker.js
@@ -3,13 +3,15 @@ $(document).on('ajaxComplete ready', function () {
     // Initialize inputs
     $('input[data-provides="anomaly.field_type.datetime"]:not([data-initialized])').each(function () {
 
-        $(this).attr('data-initialized', '')
+        var $this = $(this);
+
+        $this.attr('data-initialized', '')
             .flatpickr({
                 altInput: true,
                 allowInput: true,
-                minuteIncrement: $(this).data('step'),
-                dateFormat: $(this).data('datetime-format'),
-                enableTime: $(this).data('input-mode').indexOf('time') !== -1
+                minuteIncrement: $this.data('step'),
+                dateFormat: $this.data('datetime-format'),
+                enableTime: $this.data('input-mode').indexOf('time') !== -1
             });
     });
 });
