8def8c0fc1734880eb02070d9d3bdd1ef8969579
Martin Modler <modler@webformatik.com>
2018-06-07 13:04:40 +0200

    Allow date format: d.m.Y

--- resources/config/formats.php
+++ resources/config/formats.php
@@ -7,6 +7,7 @@ return [
         'm/d/Y'  => date('m/d/Y'), // 07/10/2015,
         'd/m/Y'  => date('d/m/Y'), // 10/07/2015,
         'Y-m-d'  => date('Y-m-d'), // 2015-07-10,
+        'd.m.Y'  => date('d.m.Y'), // 10.07.2015,
     ],
     'time' => [
         'g:i A' => date('g:i A'), // 4:00 PM,
