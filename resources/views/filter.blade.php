cf81bbe6e8adc47b5614f33916cd9257fc317767
Ryan Thompson <ryan@pyrocms.com>
2019-11-16 01:06:23 -0600

    Progress

--- resources/views/filter.blade.php
+++ resources/views/filter.blade.php
@@ -3,4 +3,4 @@
 {{ assets("styles.css", "anomaly.field_type.datetime::css/light.css") }}
 {{ assets("scripts.js", "anomaly.field_type.datetime::js/filter.js") }}
 
-<input type="text" class="form-control" data-input-mode="range" value="{{ $field_type->value }}" name="{{ $field_type->input_name }}" placeholder="{{ trans($field_type->placeholder) }}" data-datetime-format="{{ config('streams::datetime.date_format') }}" {{ html_attributes($field_type->attributes) }}>
+<input type="text" data-input-mode="range" value="{{ $fieldType->value }}" name="{{ $fieldType->input_name }}" placeholder="{{ trans($fieldType->placeholder) }}" data-datetime-format="{{ config('streams::datetime.date_format') }}" {!! html_attributes($fieldType->attributes) !!}>
