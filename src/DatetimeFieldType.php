2e6160c7ff4a283a25338b70f4979249e073c482
Ryan Thompson <ryan@pyrocms.com>
2020-02-09 18:04:54 -0600

    Meh

--- src/DatetimeFieldType.php
+++ src/DatetimeFieldType.php
@@ -137,7 +137,7 @@ class DatetimeFieldType extends FieldType
             timezone_identifiers_list()
         );
 
-        $formats = config('datetime.format.date');
+        $formats = config('streams::datetime.date_format', []);
 
         // Check for default / erroneous timezone.
         if ((!$timezone = strtolower(array_get($config, 'timezone'))) || !in_array($timezone, $timezones)) {
