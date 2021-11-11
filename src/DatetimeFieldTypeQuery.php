bbb6344513191377f1f90b3866884c037095e371
Ryan Thompson <ryan@pyrocms.com>
2021-11-11 10:16:41 -0600

    Allow single day filtering.

--- src/DatetimeFieldTypeQuery.php
+++ src/DatetimeFieldTypeQuery.php
@@ -1,4 +1,6 @@
-<?php namespace Anomaly\DatetimeFieldType;
+<?php
+
+namespace Anomaly\DatetimeFieldType;
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeQuery;
 use Anomaly\Streams\Platform\Ui\Table\Component\Filter\Contract\FilterInterface;
@@ -29,7 +31,21 @@ class DatetimeFieldTypeQuery extends FieldTypeQuery
          * Make sure the filter value
          * is something we can use.
          */
-        if (strpos($filter->getValue(), ' to ') === false) {
+        if (strpos($date = $filter->getValue(), ' to ') === false) {
+
+            $from = Carbon::createFromFormat(config('streams::datetime.date_format'), $date)
+                ->setTimezone(config('app.timezone'))
+                ->setTime(0, 0, 0) // Start at the beginning of the day.
+                ->setTimezone(config('streams::datetime.default_timezone'));
+
+            $to = Carbon::createFromFormat(config('streams::datetime.date_format'), $date)
+                ->setTimezone(config('app.timezone'))
+                ->setTime(23, 59, 59) // Include up to the end of the day.
+                ->setTimezone(config('streams::datetime.default_timezone'));
+
+            $query->whereDate($query->getQuery()->from . '.' . $filter->getField(), '>=', $from->format('Y-m-d H:i:s'));
+            $query->whereDate($query->getQuery()->from . '.' . $filter->getField(), '<=', $to->format('Y-m-d H:i:s'));
+
             return;
         }
 
@@ -37,12 +53,12 @@ class DatetimeFieldTypeQuery extends FieldTypeQuery
 
         $from = Carbon::createFromFormat(config('streams::datetime.date_format'), $from)
             ->setTimezone(config('app.timezone'))
-            ->setTime(0, 0, 0)// Start at the beginning of the day.
+            ->setTime(0, 0, 0) // Start at the beginning of the day.
             ->setTimezone(config('streams::datetime.default_timezone'));
 
         $to = Carbon::createFromFormat(config('streams::datetime.date_format'), $to)
             ->setTimezone(config('app.timezone'))
-            ->setTime(23, 59, 59)// Include up to the end of the day.
+            ->setTime(23, 59, 59) // Include up to the end of the day.
             ->setTimezone(config('streams::datetime.default_timezone'));
 
         $query->whereDate($query->getQuery()->from . '.' . $filter->getField(), '>=', $from->format('Y-m-d H:i:s'));
